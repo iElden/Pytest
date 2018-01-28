4e42e201cd5d12c4d6ea2cca8f63b382dac6555f
Etienne Bertrand <etienne.bertrand@epitech.eu>
2018-01-28 16:27:33 +0100

    updated create_allias script

--- create_alias.sh
+++ create_alias.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+chmod 755 $HOME/Pytest/pytest
 echo alias pytest="$HOME/Pytest/pytest" >> $HOME/.bashrc
 echo alias pytest="$HOME/Pytest/pytest" >> $HOME/.zshrc
 echo Alias \"pytest\" crée avec succès
